4f98174020e4facc5acafd31143d0bc6f7056811
perukas <ankarakottas@gmail.com>
2024-05-14 16:52:45 +0300

    + rvm submodule

---
+++ submodules/RobustVideoMatting/__init__.py
@@ -0,0 +1,3 @@
+import submodules.RobustVideoMatting.model as model
+import submodules.RobustVideoMatting.inference as inference
+import submodules.RobustVideoMatting.inference_utils as inference_utils
